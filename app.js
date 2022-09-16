3400ac5164d2b01b838acd4a229025f29f4c21a4
roboseb <sebastienapril@rocketmail.com>
2022-09-16 14:44:33 -0600

    Add .env file for DB URI. Remove app.js from .gitignore. Add .env to .gitignore.

---
+++ app.js
@@ -0,0 +1,74 @@
+require('dotenv').config()
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var favicon = require('serve-favicon')
+
+const formData = require('express-form-data');
+const os = require("os");
+
+const options = {
+    uploadDir: os.tmpdir(),
+    autoClean: true
+};
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+const catalogRouter = require('./routes/catalog');
+const compression = require("compression");
+const helmet = require("helmet");
+
+var app = express();
+
+//Import the mongoose module
+const mongoose = require('mongoose');
+
+//Set up default mongoose connection
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//Get the default connection
+const db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Set favicon.
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(formData.parse(options));
+app.use(compression()); // Compress all routes
+app.use(helmet());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/catalog', catalogRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
